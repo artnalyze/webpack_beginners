634934550d15b89a4c81313d70d71ef89bcc25ea
Artnalyze <artnalyze@gmail.com>
2020-07-09 09:25:10 +0700

    chapter 3 done

--- webpack_03/webpack.config.js
+++ webpack_03/webpack.config.js
@@ -77,7 +77,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|jpg|gif|svg)$/i,
+                test: /(png|jpg|gif|svg|woff2?|eot|ttf|otf|wav)(\?.*)?$/i,
                 use: [{
                         loader: "url-loader",
                         options: {
